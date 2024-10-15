64fbc140ff452939b4c7a48e7d4cf58a999200d8
Ashishchhillar-AC <chhillar960@gmail.com>
2024-10-15 22:56:31 +0530

    add one table

---
+++ ex_15102024.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class ex_15102024 {
+    public static void main(String[] args) {
+        int x =10;
+        int y =20;
+        int max = x>y ? x:y;
+        System.out.println(max);
+    }
+}
