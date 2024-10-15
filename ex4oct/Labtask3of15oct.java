5c363ab0b21026ded52693058eb2274f20558e70
Ashishchhillar-AC <chhillar960@gmail.com>
2024-10-16 00:06:15 +0530

    new request

---
+++ ex4oct/Labtask3of15oct.java
@@ -0,0 +1,11 @@
+package com.example.ex4oct;
+
+public class Labtask3of15oct {
+    public static void main(String[] args) {
+        int a =20;
+        System.out.println(--a + a++ + a--);
+        // 19+19+20=58
+        System.out.println(a);
+        //19
+    }
+}
