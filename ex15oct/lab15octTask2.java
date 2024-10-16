e8b6438434415f9d437d4dd124686271ebfdd6eb
Ashishchhillar-AC <chhillar960@gmail.com>
2024-10-17 00:05:56 +0530

    new code 1

--- ex15oct/lab15octTask2.java
+++ ex15oct/lab15octTask2.java
@@ -2,9 +2,9 @@ package com.example.ex15oct;
 
 public class lab15octTask2 {
     public static void main(String[] args) {
-        int a =40;
+        int a =10;
         System.out.println(++a + a++ +  a++);
-        //                 41+41+42 =124
+        //                 11+11+12 =34
         System.out.println(a);
     }
 }
---
+++ ex15oct/lab15octTask3.java
@@ -0,0 +1,10 @@
+package com.example.ex15oct;
+
+public class lab15octTask3 {
+    public static void main(String[] args) {
+        int a=20;
+        System.out.println(--a +a++  +a--);
+        //                 29+29+30=58
+        System.out.println(a);
+    }
+}
