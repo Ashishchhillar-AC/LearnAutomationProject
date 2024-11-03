97bd8f080bae75289c7b6ca93328568ee0625ff4
Ashishchhillar-AC <chhillar960@gmail.com>
2024-11-03 23:50:31 +0530

    new code 1

---
+++ ex_10oct2024/Lab027.java
@@ -0,0 +1,10 @@
+package com.example.ex_10oct2024;
+
+public class Lab027 {
+    public static void main(String[] args) {
+        float pi =3.14f;
+        pi =5;
+        pi =10;
+        System.out.println(pi);
+    }
+}
