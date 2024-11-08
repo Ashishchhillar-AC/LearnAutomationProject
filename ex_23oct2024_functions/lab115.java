25ebf05051f9e88dbd05fb77e04b177d412169cd
Ashishchhillar-AC <chhillar960@gmail.com>
2024-11-08 15:22:04 +0530

    new code 2

--- ex_23oct2024_functions/lab115.java
+++ ex_23oct2024_functions/lab115.java
@@ -2,6 +2,22 @@ package com.example.ex_23oct2024_functions;
 
 public class lab115 {
     public static void main(String[] args) {
+    }
+
+    public static void main(int a) {
+
+    }
+
+    public static void main(String args) {
+
+    }
+
+    public static int main() {
+        return 18;
+    }
+
+    public static String main(String a, String b) {
+        return a+b;
 
     }
 }
